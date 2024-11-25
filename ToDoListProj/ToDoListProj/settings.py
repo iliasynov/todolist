9b8e3b46fa65a4d0ea2269fcf8af98877c3c9913
iliasynov <ilias.ouandouri@ynov.com>
2024-11-25 14:57:29 +0100

    docker

--- ToDoListProj/ToDoListProj/settings.py
+++ ToDoListProj/ToDoListProj/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ToDoListProj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
